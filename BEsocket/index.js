41e56ee5b5226665d2a7b236ae72f4755e19cc85
Damon Thomas <damon22t@gmail.com>
2025-07-18 23:07:24 -0400

    Update dependencies and add socket.io

--- BEsocket/index.js
+++ BEsocket/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const port = 8888;
+const server = createServer(app);
+
+const io = new Server(server);
 
 // Enable CORS for all routes
 app.use(
@@ -12,10 +19,19 @@ app.use(
   })
 );
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
