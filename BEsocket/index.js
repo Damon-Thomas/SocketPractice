da3aa5b423c941b41dbd058251e64f502d53dcd7
Damon Thomas <damon22t@gmail.com>
2025-07-19 13:56:39 -0400

    Add SQLite dependencies and configure Docker volume for chat data
    - Updated package.json to include sqlite and sqlite3 dependencies.
    - Modified docker-compose.yaml to add a volume for persistent chat data storage.

--- BEsocket/index.js
+++ BEsocket/index.js
@@ -4,12 +4,30 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
+// open the database file
+const db = await open({
+  filename: "data/chat.db",
+  driver: sqlite3.Database,
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
 const app = express();
 const port = 8888;
 const server = createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
 // Enable CORS for all routes
 app.use(
@@ -25,10 +43,32 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+io.on("connection", async (socket) => {
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
+    io.emit("chat message", msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each(
+        "SELECT id, content FROM messages WHERE id > ?",
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (e) {
+      // something went wrong
+    }
+  }
 });
 
 server.listen(port, () => {
